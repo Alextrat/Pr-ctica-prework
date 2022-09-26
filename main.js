1a39e0dc56f25aa2b7adc46dc112708400c841c2
Alextrat <alejandro.perez@bootcamp-upgrade.com>
2022-09-26 12:13:28 +0200

    Un comentario

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var frase1="Se muestra en el navegador el este texto gracias a  document.write   "
+alert("Hola proyecto")
+document.write(frase1)
+document.write("Abajo el ejercicio 2 de Operadores")
+var nombre= "Álex";
+var apellido= "Pérez";
+var nombreCompleto = nombre +" "+ apellido;
+var edad = 38
+var mensaje = "Mi nombre es " + nombreCompleto + " y tengo " + edad + " años";
+document.write(mensaje)
